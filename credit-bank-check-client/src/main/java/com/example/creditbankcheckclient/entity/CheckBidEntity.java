75ce7d5e0876cffa2d72eddbd41fbe7cb135f333
Developer chernis77 <chernis77@gmail.com>
2023-02-28 01:11:58 +0300

    добавлены сущности CheckClientEntity и CheckBidEntity

--- credit-bank-check-client/src/main/java/com/example/creditbankcheckclient/entity/CheckBidEntity.java
+++ credit-bank-check-client/src/main/java/com/example/creditbankcheckclient/entity/CheckBidEntity.java
@@ -17,25 +17,39 @@ public class CheckBidEntity {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "bid_number")
     private String bidNumber;
 
-    private boolean  isEmployed;     // Трудоустроен или нет
+    @Column(name = "is_employed")
+    private boolean isEmployed;     // Трудоустроен или нет
 
+    @Column(name = "time_of_employment")
     private int timeOfEmployment;    // Сколько работает на данной работе в месяцах
 
+    @Column(name = "salary")
     private double salary;
 
-    private  double loanPayments;       // Ежемесячные платежи по другим кредитам
+    @Column(name = "loan_payments")
+    private double loanPayments;       // Ежемесячные платежи по другим кредитам
 
+    @Column(name = "credit_Amount")
     private double creditAmount;          //Сумма кредита в рублях
 
+    @Column(name = "credit_term")
     private double creditTerm;           //Срок кредита в месяцах - от 12 до 60
 
+    @Column(name = "percent_year")
     private double percentYear;
 
+    @Column(name = "bank_confirm")
     private Boolean bankConfirm;    // Согласие банка
 
+    @Column(name = "client_сonfirm")
     private Boolean clientConfirm;  // Согласие клиента
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_client")
+    private CheckClientEntity checkClientEntity;
 
 
 }
--- credit-bank-check-client/src/main/java/com/example/creditbankcheckclient/entity/CheckClientEntity.java
+++ credit-bank-check-client/src/main/java/com/example/creditbankcheckclient/entity/CheckClientEntity.java
@@ -1,23 +1,39 @@
 package com.example.creditbankcheckclient.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
-public class CheckClientEntity{
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "checkclient_table")
+public class CheckClientEntity {
 
-
+    @Id
+    @SequenceGenerator(name = "checkclient_tableSequence", sequenceName = "checkclient_table_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "checkclient_tableSequence")
+    @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "sure_name")
     private String surName;
 
+    @Column(name = "last_name")
     private String lastName;
 
     //    private Date birthDate;
 
+    @Column(name = "passport_num")
     private String passportNum;
 
+    @OneToOne(mappedBy = "checkClientEntity")
+    private CheckBidEntity checkBidEntity;
 
 
 }
